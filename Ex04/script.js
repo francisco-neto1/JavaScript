e6b0d3b5ebcaf2aa32cf150151acd0c0b529f5f5
francisco-neto1 <franciscon3t02018@hotmail.com>
2022-08-29 20:21:52 -0300

    6

---
+++ Ex04/script.js
@@ -0,0 +1,22 @@
+var el = document.getElementById("btVerificar");
+el.addEventListener("click", isPalindromo, false);
+
+function isPalindromo() {
+    let texto = document.getElementById('txtbox').value;
+    let textoSemEspaco = texto.replace(" ", "").toLowerCase();
+    let textInvertido = inverter(textoSemEspaco);
+
+    if (textoSemEspaco == textInvertido)
+        alert(texto + " é palíndromo") 
+    else
+        alert(texto + " não é palíndromo") 
+}
+
+function inverter(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) { 
+        newString += str[i];
+    }
+    return newString;
+
+}
